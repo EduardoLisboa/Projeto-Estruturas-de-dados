0a2086b80b63a930b51ec8b1ba8bb03a90d725c5
Fernando Lins <FernandoLins088@gmail.com>
2018-09-30 08:50:52 -0300

    remove printfs

--- Huffman/precomp.c
+++ Huffman/precomp.c
@@ -84,26 +84,17 @@ int main(unsigned short argc, unsigned char const *argv[])
 		if(freq[i])
 		{
 			enqueue(pq, create_node(i, freq[i], NULL, NULL, NULL));
-			printf("%c %d\n", i, freq[i]);
 		}
-	print_queue(pq);
 
 	while(pq->head->next)
 	{
 		Node *n1 = dequeue(pq);
 		Node *n2 = dequeue(pq);
-
 		enqueue(pq, create_node('*', n1->priority+n2->priority, NULL, n1, n2));
-		print_queue(pq);
 	}
-	printf("Tree:\n");
-	print_tree(pq->head);
-	printf("\n");
 
 	Hash_table *ht = create_hash_table(pq->head);
 
-	//printf("%u\n%u\n",trash_size(ht,freq),(8 - trash_size(ht,freq) % 8));
-	
 	unsigned short trash_size = get_trash_size(file_data_size(ht, freq));
 	unsigned short tree_size = get_tree_size(pq->head, 0);
 
@@ -225,11 +216,6 @@ void put(Hash_table *ht, unsigned char key, unsigned char *byte, unsigned short 
 {
 	unsigned short index = key;
 
-	printf("%c\n", key);
-	printf("%u\n", index);
-	printf("%s\n", byte);
-	printf("%u\n\n", size);
-
 	Element *new = malloc(sizeof(Element));
 	ht->table[index] = new;
 
@@ -241,13 +227,9 @@ void put(Hash_table *ht, unsigned char key, unsigned char *byte, unsigned short 
 unsigned char* get(Hash_table *ht, unsigned char key)
 {
 	unsigned short h = key % MAX;
-	printf("catado == %s\n", ht->table[h]->byte);
 
 	if(ht->table[h])
 		return ht->table[h]->byte;
-
-	printf("Erro get\n");
-	exit(1);
 }
 
 void search(Hash_table *ht, Node *tree, unsigned char *byte, unsigned short size)
@@ -304,14 +286,11 @@ unsigned short get_tree_size(Node *tree, unsigned short size)
 
 void create_header(FILE *out, unsigned short trash, unsigned short tree)
 {
-	//printf("trash %hu\ntree %hu\n", trash, tree);
 	unsigned char byte1, byte2, aux = tree >> 8;
 	byte1 = trash << 5;
 	byte1 = byte1 | (tree >> 8);
 	byte2 = (tree << 8) >> 8;
 
-	printf("\n%hu\n", byte1);
-	printf("%hu\n", byte2);
 	fprintf(out, "%c", byte1);
 	fprintf(out, "%c", byte2);
 }
@@ -335,24 +314,19 @@ void compress(FILE *input_file, FILE *out, Hash_table *ht, unsigned short size)
 
 	while(byte_count < size/8)
 	{
-		printf("lp infinito? %hu\n", byte_count);
 		if(there_is_rest)
 		{
 			there_is_rest = 0;
-			printf("rest\n");
 			strcpy(temp, rest);
 			memset(rest, '\0', 8);
 		}
 		else
 		{
-			printf("else\n");
 			ch = getc(input_file);
-			printf("lido == %c\n", ch);
 			strcpy(temp, get(ht, ch));
 		}
 
 		new_byte_size = strlen(temp) + strlen(byte);
-		printf("new_byte_size == %hu\n", new_byte_size);
 
 		if(new_byte_size == 8)
 		{
@@ -368,7 +342,6 @@ void compress(FILE *input_file, FILE *out, Hash_table *ht, unsigned short size)
 			{
 					rest[j] = temp[i];
 			}
-			printf("resto %s\n", rest);
 			temp[8-strlen(byte)] = '\0';
 
 			strcat(byte, temp);
@@ -383,8 +356,6 @@ void compress(FILE *input_file, FILE *out, Hash_table *ht, unsigned short size)
 	}
 	if(there_is_rest)
 	{
-		printf("last byte %lu\n", strlen(rest));
-		printf("rest = %s\n", rest);
 		strcat(byte, rest);
 		for(i=strlen(rest)%8; i<8; i++)
 			rest[i] = '0';
@@ -395,7 +366,6 @@ void compress(FILE *input_file, FILE *out, Hash_table *ht, unsigned short size)
 
 void write_byte(FILE *out, unsigned char* byte_str)
 {
-	printf("byte str = %s\n", byte_str);
 	unsigned short i;
 	unsigned char byte = 0;
 	
@@ -406,7 +376,6 @@ void write_byte(FILE *out, unsigned char* byte_str)
 			byte = set_bit(byte, (8-i)-1);
 		}
 	}
-	printf("byte!!!! %hu\n", byte);
 	fprintf(out, "%c", byte);
 }
 
@@ -415,67 +384,3 @@ unsigned char set_bit(unsigned char c, unsigned short i)
 	unsigned char mask = 1 << i;
 	return mask | c;
 }
-
-
-
-/*
-void compress(FILE *input_file, FILE *out, Hash_table *ht, unsigned short size)
-{
-	rewind(input_file);
-	printf("size = %hu\n", size/8);
-
-	unsigned short byte_count = 0;
-	unsigned char rest[8];
-
-	while(byte_count < size/8)
-	{
-		strcpy(rest, select_byte(input_file, out, ht,rest));
-		if(rest)
-	}
-}
-
-unsigned char* select_byte(FILE *input_file, FILE *out, Hash_table *ht, unsigned char *rest)
-{
-	unsigned char byte[8], temp[8], ch;
-	unsigned short new_size = 0, i, j;
-
-	while(strlen(byte) != 8)
-	{
-		ch = getc(input_file);
-		strcpy(temp, get(ht, ch));
-		new_size = (strlen(temp) + strlen(byte));
-
-		if(new_size > 8)
-		{
-			for(i=new_size-8, j=0; i>strlen(temp)+1; i++, j++)
-			{
-				rest[j] = temp[i];
-			}
-			//strcpy(rest, temp[new_size-8];
-			temp[new_size-8] = '\0';
-			strcpy(byte, temp);
-			write_byte(input_file, byte);
-			return rest;
-		}
-	}
-	write_byte(input_file, byte);
-	return NULL;
-}
-
-void put_in_file(const char *string, FILE *out)
-{
-    unsigned short i;
-
-    unsigned char byte = 0;
-
-    for(i = 0; i < 8; i++)
-    {
-        if(string[i] == '1')
-        {
-            byte |= 1 << (7-i);
-        }
-    }
-
-    fprintf(out, "%c", byte);
-}
-*/
